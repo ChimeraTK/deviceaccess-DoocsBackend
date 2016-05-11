507bd04c392591128a776d8d89edcf70b3f575a8
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-05-11 08:47:20 +0000

    fixed extraction of DOOCS version from library symlink if its target contains a directory path
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess-DoocsBackend/trunk@2849 5f601a5c-88b9-47f3-b851-818f6741d00d

--- cmake/Modules/FindDOOCS.cmake
+++ cmake/Modules/FindDOOCS.cmake
@@ -41,7 +41,7 @@ set(DOOCS_LINK_FLAGS "-Wl,--no-as-needed")
 
 # extract DOOCS version from librar so symlink. Note: This is platform dependent and only works
 # if DOOCS was installed from the Debian pagackes. Find a better version detection scheme!
-execute_process(COMMAND bash -c "readlink ${DOOCS_DIR}/libDOOCSapi.so | sed -e 's/libDOOCSapi.so.//' -e 's/-.*$//'" OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE DOOCS_VERSION)
+execute_process(COMMAND bash -c "readlink ${DOOCS_DIR}/libDOOCSapi.so | sed -e 's/^.*libDOOCSapi.so.//' -e 's/-.*$//'" OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE DOOCS_VERSION)
 
 # use a macro provided by CMake to check if all the listed arguments are valid and set DOOCS_FOUND accordingly
 include(FindPackageHandleStandardArgs)
