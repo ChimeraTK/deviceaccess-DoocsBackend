4af336c649fd46a8a494fc990a7d1ba8e9318bfa
Martin Hierholzer <martin.hierholzer@desy.de>
2016-12-08 16:51:53 +0100

    - do not push linking against the DOOCS libraries to the application - add necessary symbols for DOOCS server library

---
+++ src/EmptyDoocsFunctions.cc
@@ -0,0 +1,38 @@
+#include <eq_fct.h>
+
+void interrupt_usr1_prolog(int) {
+}
+
+void interrupt_usr1_epilog(int) {
+}
+
+void interrupt_usr2_prolog(void) {
+}
+
+void interrupt_usr2_epilog(void) {
+}
+
+void post_init_prolog(void) {
+}
+
+void post_init_epilog(void) {
+}
+
+void eq_cancel(void) {
+}
+
+void refresh_prolog() {
+}
+
+void refresh_epilog() {
+}
+
+void eq_init_prolog() {
+}
+
+void eq_init_epilog() {
+}
+
+EqFct *eq_create(int, void *) {
+  return nullptr;
+}
