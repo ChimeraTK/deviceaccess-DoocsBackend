3eb5a82405e3a6e856f3c56f956119e9a3d25f21
Jens Georg <jens.georg@desy.de>
2019-03-27 16:15:14 +0100

    Declare DOOCS dummy functions as weak
    So if we link against the DeviceAccess DOOCS backend and the
    DOOCS ControlsystemAdapter the ControlsystemAdapter symbols always win.
    
    Also make the functions assert (in Debug mode) if the the functions are
    called accidentally

--- src/EmptyDoocsFunctions.cc
+++ src/EmptyDoocsFunctions.cc
@@ -1,27 +1,64 @@
+#include <cassert>
 #include <eq_fct.h>
 
-void interrupt_usr1_prolog(int) {}
+__attribute__((weak)) void interrupt_usr1_prolog(int) {
+  // Code should not be reached
+  assert(false);
+}
 
-void interrupt_usr1_epilog(int) {}
+__attribute__((weak)) void interrupt_usr1_epilog(int) {
+  // Code should not be reached
+  assert(false);
+}
 
-void interrupt_usr2_prolog(void) {}
+__attribute__((weak)) void interrupt_usr2_prolog(void) {
+  // Code should not be reached
+  assert(false);
+}
 
-void interrupt_usr2_epilog(void) {}
+__attribute__((weak)) void interrupt_usr2_epilog(void) {
+  // Code should not be reached
+  assert(false);
+}
 
-void post_init_prolog(void) {}
+__attribute__((weak)) void post_init_prolog(void) {
+  // Code should not be reached
+  assert(false);
+}
 
-void post_init_epilog(void) {}
+__attribute__((weak)) void post_init_epilog(void) {
+  // Code should not be reached
+  assert(false);
+}
 
-void eq_cancel(void) {}
+__attribute__((weak)) void eq_cancel(void) {
+  // Code should not be reached
+  assert(false);
+}
 
-void refresh_prolog() {}
+__attribute__((weak)) void refresh_prolog() {
+  // Code should not be reached
+  assert(false);
+}
 
-void refresh_epilog() {}
+__attribute__((weak)) void refresh_epilog() {
+  // Code should not be reached
+  assert(false);
+}
 
-void eq_init_prolog() {}
+__attribute__((weak)) void eq_init_prolog() {
+  // Code should not be reached
+  assert(false);
+}
 
-void eq_init_epilog() {}
+__attribute__((weak)) void eq_init_epilog() {
+  // Code should not be reached
+  assert(false);
+}
 
-EqFct* eq_create(int, void*) {
+__attribute__((weak)) EqFct* eq_create(int, void*) {
+  // Code should not be reached
+  assert(false);
+
   return nullptr;
 }
