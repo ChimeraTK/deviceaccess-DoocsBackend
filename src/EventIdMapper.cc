990af4f9fcede70195186bf07538d2fef870a5bc
Jens Georg <mail@jensge.org>
2020-03-19 14:17:52 +0100

    Fix missing template argument
    For whatever reason, works with g++

--- src/EventIdMapper.cc
+++ src/EventIdMapper.cc
@@ -1,6 +1,6 @@
 #include "EventIdMapper.h"
 
 ChimeraTK::VersionNumber EventIdMapper::getVersionForEventId(const doocs::EventId& eventId) {
-  std::lock_guard locker{_mapMutex};
+  std::lock_guard<decltype(_mapMutex)> locker{_mapMutex};
   return _eventIdToVersionMap[eventId];
 }
