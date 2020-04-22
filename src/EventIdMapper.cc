fad14561c916168753789fcbc16ff2f20fe46d99
Martin Hierholzer <martin.hierholzer@desy.de>
2020-04-22 16:46:34 +0200

    generate new version for eventId == 0

--- src/EventIdMapper.cc
+++ src/EventIdMapper.cc
@@ -1,6 +1,7 @@
 #include "EventIdMapper.h"
 
 ChimeraTK::VersionNumber EventIdMapper::getVersionForEventId(const doocs::EventId& eventId) {
+  if(eventId.to_int() == 0) return {}; // event id will be 0 e.g. if value was written from the panel
   std::lock_guard<decltype(_mapMutex)> locker{_mapMutex};
   return _eventIdToVersionMap[eventId];
 }
