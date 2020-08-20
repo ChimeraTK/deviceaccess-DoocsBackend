e7a16a7cc5f84baef2bf7665dae385cb1743cb42
Nadeem <nadeem.shehzad@desy.de>
2020-08-20 17:32:22 +0200

    improved cout messages in doPostRead.

--- include/DoocsBackendRegisterAccessor.h
+++ include/DoocsBackendRegisterAccessor.h
@@ -118,14 +118,14 @@ namespace ChimeraTK {
       if(dst.get_event_id() <= _lastEventId) {
         //TransferElement::_versionNumber = VersionNumber();
         TransferElement::setDataValidity(DataValidity::faulty);
-        std::cout << "warning, eventId smaller than the last vaild eventId" << std::endl;
+        std::cout << "warning, "<< _path <<" current eventId "<<dst.get_event_id()<<" is not bigger than the last vaild eventId " <<_lastEventId<< std::endl;
         return;
       }
 
       auto newVersionNumber = EventIdMapper::getInstance().getVersionForEventId(dst.get_event_id());
       if(newVersionNumber < TransferElement::_versionNumber) {
         TransferElement::setDataValidity(DataValidity::faulty);
-        std::cout << "warning, eventId smaller than the last vaild eventId" << std::endl;
+        std::cout << "warning, "<< _path <<" newVersionNumber "<<std::string(newVersionNumber)<<" smaller than the last vaild versionNumber " <<std::string(TransferElement::_versionNumber)<< std::endl;
         return;
       }
       TransferElement::_versionNumber = newVersionNumber;
